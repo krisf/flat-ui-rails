1a92cee716fe473a39ffb5b640fb3be47a07eaa1
Kris Forbes <kris@gohosted.ca>
2013-05-22 18:36:10 -0400

    added flat-ui javascript to project

--- lib/flat-ui-rails/version.rb
+++ lib/flat-ui-rails/version.rb
@@ -1,5 +1,5 @@
 module FlatUi
   module Rails
-    VERSION = "0.0.6"
+    VERSION = "0.1.0"
   end
 end
---
+++ vendor/assets/javascripts/flat-ui.js
@@ -0,0 +1,10 @@
+//= require jquery-ui-1.10.3.custom.min
+//= require jquery.ui.touch-punch.min
+//= require jquery.placeholder
+//= require jquery.stackable
+//= require jquery.tagsinput
+//= require bootstrap-select
+//= require bootstrap-switch
+//= require flatui-checkbox
+//= require flatui-radio
+//= require flatui-general
